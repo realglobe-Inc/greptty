bf1c5d6c8ce6023279ed0164a5eaf2b26a3306c0
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-09-16 18:40:09 +0900

    feat: enumerate /dev/*

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!!")
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let mut entries = fs::read_dir("/dev/")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+    entries.sort();
+
+    println!("{:?}", entries);
+    Ok(())
 }
