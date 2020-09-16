d2338fecde6bc6ef0e3ac55904c3724032ff1aa5
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-09-16 19:41:24 +0900

    feat: filter paths

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::{fs, io};
 
 fn main() -> io::Result<()> {
-    let mut entries = fs::read_dir("/dev/")?
+    let mut entries = fs::read_dir("/dev/").unwrap()
         .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
+        .map(|path| path.unwrap().into_os_string().into_string().unwrap())
+        .filter(|path| path.starts_with("/dev/ttyACM"))
+        .collect::<Vec<_>>();
     entries.sort();
 
     println!("{:?}", entries);
