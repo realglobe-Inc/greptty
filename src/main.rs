e68cc9adb741791c3e8832174ba3291bf68aed03
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-09-16 21:21:08 +0900

    feat: set baud rate

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::{fs, io};
+use std::process::Command;
 
 fn main() -> io::Result<()> {
-    let mut entries = fs::read_dir("/dev/").unwrap()
+    // FIXME: this code includes many unwrap()
+    let port_names = fs::read_dir("/dev/").unwrap()
         .map(|res| res.map(|e| e.path()))
         .map(|path| path.unwrap().into_os_string().into_string().unwrap())
         .filter(|path| path.starts_with("/dev/ttyACM"))
         .collect::<Vec<_>>();
-    entries.sort();
 
-    println!("{:?}", entries);
+    for port_name in port_names {
+        set_baud_rate(port_name, 9600)
+    }
+
     Ok(())
+}
+
+fn set_baud_rate(port_name: String, baud_rate: u32) {
+    Command::new("stty")
+            .arg(format!("-F {} raw {}", port_name, baud_rate))
+            .output()
+            .expect("error");
 }
