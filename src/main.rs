3248bd0178e228a558bf599b1a90e0cf506e75d7
Akihito KIRISAKI <kirisaki@klara.works>
2020-09-17 18:23:07 +0900

    feat: read from serial port

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::{fs, io};
+use std::io::BufReader;
 use std::process::Command;
-use std::fs::File;
+use tokio::fs::File;
 use std::io::prelude::*;
 
-fn main() -> io::Result<()> {
+use tokio::prelude::*;
+use tokio::stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // FIXME: this code includes many unwrap()
     let port_names = fs::read_dir("/dev/").unwrap()
         .map(|res| res.map(|e| e.path()))
@@ -13,17 +18,21 @@ fn main() -> io::Result<()> {
 
     for port_name in port_names {
         set_baud_rate(port_name.clone(), 9600);
-        let mut port = File::open(port_name).expect("can't open port");
+        let port = File::open(port_name).await?;
+        let mut stream= tokio::io::BufReader::new(port);
 
-        let mut contents = String::new();
-        port.read_to_string(&mut contents).expect("something wrong");
-        println!("{}", contents);
+        loop {
+            let mut buf = String::new();
+            stream.read_line(&mut buf).await?;
+            print!("{}", buf);
+        }
     }
 
     Ok(())
 }
 
 fn set_baud_rate(port_name: String, baud_rate: u32) {
+    // TODO: change to better way to set baud rate
     Command::new("stty")
             .arg(format!("-F {} raw {}", port_name, baud_rate))
             .output()
